a7be23bfb649bd92740488c60f534b97d1f889bd
proprocastinator <99007672+proprocastinator@users.noreply.github.com>
2022-05-17 17:23:49 +0530

    Add files via upload

---
+++ selfassessment.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int prev,curr,len=0;
+    int maxlen=0;
+    scanf("%d",&prev);
+    if(prev!=-1)
+    {
+        len=1;maxlen=1;
+        scanf("%d",&curr);
+        while(curr!=-1)
+        {
+            if(prev<curr)
+            {
+                len=len+1;
+            }
+            else
+                if(maxlen<len)
+                {
+                    maxlen=len;
+                }
+                prev=curr;
+                scanf("%d",&curr);
+        }
+            if(maxlen<len)
+            {
+                maxlen=len;
+            }
+    }
+    printf("%d",maxlen);
+    return 0;
+}
