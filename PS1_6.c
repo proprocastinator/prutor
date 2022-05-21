52f821cd19ab33ee156e74c4bdb1a7ea519888da
proprocastinator <99007672+proprocastinator@users.noreply.github.com>
2022-05-21 11:16:11 +0530

    Update PS1_6.c

--- PS1_6.c
+++ PS1_6.c
@@ -1,3 +1,43 @@
+// ## Problem Statement
+
+
+// Mahesh likes to walk in the evening and keep record of how much he walks every day around the park (Walks along the perimeters of the park). There are three parks with shapes of square, rectangle and triangle. Mahesh has record of length of sides of the parks and the number of rounds he makes every day.
+
+// Please help Mahesh find the distance that he has walked. The structure of information is as follows:
+
+  
+
+// Shape 1 indicates square
+
+// Shape 2 indicates rectangle
+
+// Shape 3 indicates triangle
+
+// All the length parameters are in meters. You can read the information as input from the `stdin`. The number of inputs is dependent on the first entry of the input which determines the shape.
+
+// Please write a `c code` that takes the information as input and prints the total distance that Mahesh has walked. All the inputs are positive integers.
+
+
+// ## Input Format
+// First line decides the shape of the park.
+// Second line determines the number of rounds that Mahesh has made around the park.
+// The third line contains the information of the side lengths of given shape.
+//   In case of square shaped park, there is only one entry as all the sides equal.
+//   In case of rectangle shaped park, there are two entries.
+//   In case of triangle shaped park, there are three entries.
+
+
+// shape
+// rounds
+// side1 side2 side3
+
+// ## Sample Input:
+// ```
+// 2
+// 10
+// 3 4
+
+// Output: 140
 #include <stdio.h>
 
 int main(void) {
